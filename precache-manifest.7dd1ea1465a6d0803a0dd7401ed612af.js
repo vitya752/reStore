699d09ffeecf67b947b71ae879119c2767794756
vitya752 <vitya752@ukr.net>
2019-10-09 17:48:33 +0300

    Updates

---
+++ precache-manifest.7dd1ea1465a6d0803a0dd7401ed612af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568e4bef5e2cb399d76b9a702157b585",
+    "url": "/reStore/index.html"
+  },
+  {
+    "revision": "118e7693e577e34451a1",
+    "url": "/reStore/static/css/main.02fba0dd.chunk.css"
+  },
+  {
+    "revision": "f100e250a4dc0c525d38",
+    "url": "/reStore/static/js/2.933cae8b.chunk.js"
+  },
+  {
+    "revision": "118e7693e577e34451a1",
+    "url": "/reStore/static/js/main.fab17021.chunk.js"
+  },
+  {
+    "revision": "97cccd05e6af67575af7",
+    "url": "/reStore/static/js/runtime-main.40941a94.js"
+  }
+]);
